aae3d92d0d5a618dcb0c673d5a267883f3a19207
Orimous <114757440+Orimous@users.noreply.github.com>
2023-03-25 15:02:32 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+const menu = document.querySelector('.burger__menu__body')
+const menuBtn = document.querySelector('.burger__menu__icon')
+
+const body = document.body;
+
+if (menu && menuBtn) {
+    menuBtn.addEventListener('click', e => {
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+        body.classList.toggle('lock')
+    })
+
+    menu.addEventListener('click', e => {
+        if (e.target.classList.contains('menu__body')) {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+            body.classList.remove('lock')
+        }
+    })
+
+    menu.querySelectorAll('.burger__menu__link').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+            body.classList.remove('lock')
+        })
+    })
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+    anchor.addEventListener('click', event => {
+        event.preventDefault();
+
+        const blockID = anchor.getAttribute('href').substring(1);
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
+
+
 var typed = new Typed(".multipleText", {
     strings: ["Frontend Developer", "Android Developer", "Designer"],
     typeSpeed: 100,
